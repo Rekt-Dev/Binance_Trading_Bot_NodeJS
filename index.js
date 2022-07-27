dc5cb89f69672a26243c4c8b9f177d29c86490cb
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-27 08:54:39 +0300

    params

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 require("dotenv").config;
 
+const ccxt = require("ccxt");
+const axios = require("axios");
+
+const tick = async () => {};
+
+const run = () => {
+  const config = {
+    asset: "BTC",
+    base: "USDT",
+    allocation: 0.1,
+    spread: 0.2,
+    tickInterval: 2000
+  };
+};
+
 /* var http = require("http");
 
 //create a server object:
